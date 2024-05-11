d13e259189f98fea49963a41f3e031dcc9db8240
wdghzym <67120670+wdghzym@users.noreply.github.com>
2024-05-11 10:51:37 +0800

    Initial commit

---
+++ BenchDeploy/BenchDeploy.cs
@@ -0,0 +1,45 @@
+﻿using Modding;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UObject = UnityEngine.Object;
+
+namespace BenchDeploy
+{
+    public class BenchDeploy : Mod
+    {
+        internal static BenchDeploy Instance;
+
+        //public override List<ValueTuple<string, string>> GetPreloadNames()
+        //{
+        //    return new List<ValueTuple<string, string>>
+        //    {
+        //        new ValueTuple<string, string>("White_Palace_18", "White Palace Fly")
+        //    };
+        //}
+
+        //public BenchDeploy() : base("BenchDeploy")
+        //{
+        //    Instance = this;
+        //}
+
+        public override string GetVersion()
+        {
+            var version = GetType().Assembly.GetName().Version.ToString();
+#if DEBUG
+            version += "-debug";
+#endif
+            return version;
+        }
+
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+        {
+            Log("Initializing");
+
+            Instance = this;
+
+            Log("Initialized");
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# BenchDeploy
+
+A mod for the game Hollow Knight.
